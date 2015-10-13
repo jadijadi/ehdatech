a806180939b84b56582c4a6543a9913638834b6f
jadijadi <jadijadi@gmail.com>
2015-10-13 17:07:57 +0330

    start

---
+++ assets/js/custom.js
@@ -0,0 +1,30 @@
+﻿
+/*=============================================================
+    Authour URL: www.designbootstrap.com
+
+    http://www.designbootstrap.com/
+
+    License: MIT
+
+    http://opensource.org/licenses/MIT
+
+    100% Free To use For Personal And Commercial Use.
+
+    IN EXCHANGE JUST TELL PEOPLE ABOUT THIS WEBSITE
+   
+========================================================  */
+
+$(document).ready(function () {
+    // SCROLL SCRIPTS 
+        $('.scroll-me a').bind('click', function (event) { //just pass scroll-me class and start scrolling
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutQuad');
+        event.preventDefault();
+        });
+    // BACKGROUND VIDEO SCRIPTS
+        $(function () {
+            $(".player").mb_YTPlayer(); // .player - class to add for playing video ( see the div above to understand)
+        });
+});
